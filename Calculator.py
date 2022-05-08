bdd011da21fb54c52bf2c28412c176585c7a2661
HujaifaX <97424865+HujaifaX@users.noreply.github.com>
2022-05-08 09:28:19 +0600

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -24,18 +24,18 @@ print('4.Divide')
 
 # Take input from the user 
 choice = input('Enter choice(1/2/3/4):')
-num1 = int(input('Enter first number: ')
+num1 = int(input('Enter first number: '))
 num2 = int(input('Enter second number: '))
 if choice == '1': 
-print(num1,'+', ,num2,'=', add(num1,num2))
+	print(num1,'+', ,num2,'=', add(num1,num2))
 
 elif choice == '2':
-print(num1,'-',num2,'=', subtract(num1,num2))
+	print(num1,'-',num2,'=', subtract(num1,num2))
 
 elif choice == ¹3':
-print(num1,'*',num2,'=', multiply(num1,num2))
+	print(num1,'*',num2,'=', multiply(num1,num2))
 
 elif choice == '4':
-print(num1,'/',num2,'=', divide(num1,num2))
+	print(num1,'/',num2,'=', divide(num1,num2))
 
 else: print("Invalid input')
